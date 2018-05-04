df571af094291689a5389336f462e3aab20c1e3d
condac <burns.nu@gmail.com>
2018-05-04 12:10:00 +0200

    Working encoderprogram for python

--- encoderProgram/python/encode.py
+++ encoderProgram/python/encode.py
@@ -1,100 +1,200 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--foo', help='foo help')
 parser.add_argument("--hex", help="Input hex strings to decode Ex: 0xff, 0xde, 0xed")
+parser.add_argument("--status-nr", help="Input the number status message counters (1-7)")
+parser.add_argument("number", help="Number to encode to AMB format")
 args = parser.parse_args()
 
 get_bin = lambda x, n: format(x, 'b').zfill(n)
 
-words = args.hex.split(",")
+print(args)
+
+def hexString(instring,offset) :
+    offset1 = (offset-1)*8
+    offset2 = (offset)*8
+    instring = instring[offset1:offset2]
+    value = int(instring,2)
+    out = "0x"+format(value, '02X')
+
+    return out
+def encodeNumber() :
+    number = int(args.number)
+    if (args.status_nr == None):
+        ghost = 0
+    else:
+        ghost = int(args.status_nr)
+
+    ghost = get_bin(ghost, 7) # the fist bit in the counters is always 0, so we take 7 bits and add a 0 to the end
+    #print(ghost)
+    ghostbits = ghost+"0"
+    #for i in range ( len ( ghost ) ): # Reverse bit order
+    #    ghostbits = ghost[i]+ghostbits
+    print(ghostbits)
+    encode = get_bin(number,24) #Convert to binary string 24bit long
+    print(encode)
+    encode2 = "";
+    countunknown = 0;
+    for i in range ( len ( encode ) ): # Reverse bit order and insert "ghostbits" in every 4th position
+        encode2 = encode[i]+encode2
+        if (i==2 or i==5 or i==8 or i==11 or i==14 or i==17 or i==20 or i==23 or i==26 or i==29) :
+            encode2 = ""+ghostbits[countunknown]+encode2
+            countunknown = countunknown+1
+    #print(encode2)
+
+    #Here comes the magic part...
+
+    encode2 = encode2+"00000000"+"00000000"; #Add the d9 d10 data that is always 0
+
+    encodeMatrix = "00000000000000000000000000000000000000"+encode2; # 36 zero + message
+    #print("      encode2: "+encode2);
+    encodeOut = "";
+    i = 36
+    while (i<len(encodeMatrix)-1) :
+
+        temp = (""+encodeMatrix[i]+
+                encodeMatrix[i-1]+
+                encodeMatrix[i-2]+
+                encodeMatrix[i-3]+
+                encodeMatrix[i-9]+
+                encodeMatrix[i-14]+
+                encodeMatrix[i-15]+
+                encodeMatrix[i-17]+
+                encodeMatrix[i-18]+
+                encodeMatrix[i-19]+
+                encodeMatrix[i-21]+
+                encodeMatrix[i-22]+
+                encodeMatrix[i-23] )
+
+        count1 = 0;
+
+        for y in range ( len ( temp ) ):
+            if ( temp[y]=='1') :
+                count1=count1+1
 
 
+        if ( (count1 & 1) == 0 ) :
+            # even... puts out 0
+            if (encodeMatrix[i-1] == '1') :
+                encodeOut+="01";
+            else :
+                encodeOut+="00";
 
-numbers = []
-numbersBinary = ""
-for word in words :
-    numbers.append(int(word,16))
-    numbersBinary = numbersBinary + get_bin(int(word,16),8)
-
-
-decode = ""
-for index in range ( len ( numbersBinary ) ):
-    if (index % 2 != 0) :
-
-        if ( numbersBinary[index-1] == numbersBinary[index] ) :
-            decode+="0"
 
         else :
-            decode+="1"
+            # odd... puts out 1
+            if (encodeMatrix[i-1] == "1") :
+                encodeOut+="10";
+            else :
+                encodeOut+="11";
+        i = i+1
+    encodeOut = encodeOut[6:]
+    print(encodeOut)
 
 
 
-if ( decode[:8] == "00110111") :
-    print("Valid header found")
-    ifhead = 8
-else :
-    print("No header or invalid header")
-    ifhead = 0
-slask = ""
+    print("....................pre1  pre2  d1    d2    d3    d4    d5    d6    d7    d8    d9    d10")
+    print("                  { 0xF9", "0x16", hexString(encodeOut,1), hexString(encodeOut,2),
+                                            hexString(encodeOut,3), hexString(encodeOut,4),
+                                            hexString(encodeOut,5), hexString(encodeOut,6),
+                                            hexString(encodeOut,7), hexString(encodeOut,8),
+                                            hexString(encodeOut,9), hexString(encodeOut,10)+ " }", sep=', ')
 
-ghost = ""
+def decodeHex() :
+    words = args.hex.split(",")
 
-ghost = decode[ifhead+0]+ghost
-
-slask = decode[ifhead+1]+slask
-slask = decode[ifhead+2]+slask
-slask = decode[ifhead+3]+slask
-
-ghost = decode[ifhead+4]+ghost
-
-slask = decode[ifhead+5]+slask
-slask = decode[ifhead+6]+slask
-slask = decode[ifhead+7]+slask
-
-ghost = decode[ifhead+8]+ghost
-
-slask = decode[ifhead+9]+slask
-slask = decode[ifhead+10]+slask
-slask = decode[ifhead+11]+slask
-
-ghost = decode[ifhead+12]+ghost
-
-slask = decode[ifhead+13]+slask
-slask = decode[ifhead+14]+slask
-slask = decode[ifhead+15]+slask
-
-ghost = decode[ifhead+16]+ghost
-
-slask = decode[ifhead+17]+slask
-slask = decode[ifhead+18]+slask
-slask = decode[ifhead+19]+slask
-
-ghost = decode[ifhead+20]+ghost
-
-slask = decode[ifhead+21]+slask
-slask = decode[ifhead+22]+slask
-slask = decode[ifhead+23]+slask
-
-ghost = decode[ifhead+24]+ghost
-
-slask = decode[ifhead+25]+slask
-slask = decode[ifhead+26]+slask
-slask = decode[ifhead+27]+slask
-
-ghost = decode[ifhead+28]+ghost
-
-slask = decode[ifhead+29]+slask
-slask = decode[ifhead+30]+slask
-slask = decode[ifhead+31]+slask
+    numbers = []
+    numbersBinary = ""
+    for word in words :
+        numbers.append(int(word,16))
+        numbersBinary = numbersBinary + get_bin(int(word,16),8)
 
 
-foo = int(slask, 2)
-print(" binary : "+numbersBinary)
-print(" binary : ", sep='', end='')
-for index in range ( len ( decode ) ):
-    print(";", sep='', end='')
-    print(decode[index], sep='', end='')
-print(" ")
-print(" decoded: "+decode)
-print(" decoded int: "+str(foo) )
-print(" ghost: "+ghost)
+    decode = ""
+    for index in range ( len ( numbersBinary ) ):
+        if (index % 2 != 0) :
+
+            if ( numbersBinary[index-1] == numbersBinary[index] ) :
+                decode+="0"
+
+            else :
+                decode+="1"
+
+
+
+    if ( decode[:8] == "00110111") :
+        print("Valid header found")
+        ifhead = 8
+    else :
+        print("No header or invalid header")
+        ifhead = 0
+    slask = ""
+
+    ghost = ""
+
+    ghost = decode[ifhead+0]+ghost
+
+    slask = decode[ifhead+1]+slask
+    slask = decode[ifhead+2]+slask
+    slask = decode[ifhead+3]+slask
+
+    ghost = decode[ifhead+4]+ghost
+
+    slask = decode[ifhead+5]+slask
+    slask = decode[ifhead+6]+slask
+    slask = decode[ifhead+7]+slask
+
+    ghost = decode[ifhead+8]+ghost
+
+    slask = decode[ifhead+9]+slask
+    slask = decode[ifhead+10]+slask
+    slask = decode[ifhead+11]+slask
+
+    ghost = decode[ifhead+12]+ghost
+
+    slask = decode[ifhead+13]+slask
+    slask = decode[ifhead+14]+slask
+    slask = decode[ifhead+15]+slask
+
+    ghost = decode[ifhead+16]+ghost
+
+    slask = decode[ifhead+17]+slask
+    slask = decode[ifhead+18]+slask
+    slask = decode[ifhead+19]+slask
+
+    ghost = decode[ifhead+20]+ghost
+
+    slask = decode[ifhead+21]+slask
+    slask = decode[ifhead+22]+slask
+    slask = decode[ifhead+23]+slask
+
+    ghost = decode[ifhead+24]+ghost
+
+    slask = decode[ifhead+25]+slask
+    slask = decode[ifhead+26]+slask
+    slask = decode[ifhead+27]+slask
+
+    ghost = decode[ifhead+28]+ghost
+
+    slask = decode[ifhead+29]+slask
+    slask = decode[ifhead+30]+slask
+    slask = decode[ifhead+31]+slask
+
+
+    foo = int(slask, 2)
+    print(" binary : "+numbersBinary)
+    print(" binary : ", sep='', end='')
+    for index in range ( len ( decode ) ):
+        print(";", sep='', end='')
+        print(decode[index], sep='', end='')
+    print(" ")
+    print(" decoded: "+decode)
+    print(" decoded int: "+str(foo) )
+    print(" ghost: "+ghost)
+
+
+if (args.hex != None) :
+    print("decoding hex string")
+    decodeHex()
+if (args.number != None) :
+    print("Encoding number")
+    encodeNumber()
--- openAST_Arduino/openAST_Arduino.ino
+++ openAST_Arduino/openAST_Arduino.ino
@@ -24,11 +24,11 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
- 
+
 char timerstop = 0b01010000;
 char timerstart = 0b01010001;
 
-boolean sendStatus = false;
+boolean sendStatus = false; // Set to true if you have status messages 1-7 that you know work
 
 // 2351957
 // ....................pre1  pre2  d1    d2    d3    d4    d5    d6    d7    d8    d9    d10
@@ -41,12 +41,21 @@ char Status5[12]   = { 0xF9, 0x16, 0x36, 0x55, 0x57, 0x09, 0xFB, 0x3F, 0x91, 0x2
 char Status6[12]   = { 0xF9, 0x16, 0x0E, 0xFE, 0xF0, 0x8A, 0x22, 0x3C, 0x52, 0x1B, 0x3F, 0xF3 };
 char Status7[12]   = { 0xF9, 0x16, 0xD7, 0xA8, 0x10, 0x77, 0xD1, 0x23, 0xA2, 0xD7, 0xC3, 0x3C };
 
+//Status messages for custom numbers. Not sure if these work or just makes things worse. To use set sendStatus to true a few lines up
+//char Status1[12]   = { 0xF9, 0x16, 0x03, 0xBC, 0x00, 0xC0, 0x3C, 0xFF, 0x40, 0x00, 0xC0, 0x3C };
+//char Status2[12]   = { 0xF9, 0x16, 0x00, 0x03, 0xBC, 0x00, 0xC0, 0x3F, 0x40, 0xFC, 0xC0, 0x3C };
+//char Status3[12]   = { 0xF9, 0x16, 0x03, 0xBF, 0xBC, 0xC0, 0xFC, 0xC3, 0xBC, 0xFC, 0xC0, 0x3C };
+//char Status4[12]   = { 0xF9, 0x16, 0x00, 0x00, 0x03, 0xBC, 0x00, 0xC3, 0x80, 0xFC, 0x3C, 0x3C };
+//char Status5[12]   = { 0xF9, 0x16, 0x03, 0xBC, 0x03, 0x7C, 0x3C, 0x3F, 0x7C, 0xFC, 0x3C, 0x3C };
+//char Status6[12]   = { 0xF9, 0x16, 0x00, 0x03, 0xBF, 0xBC, 0xC0, 0xFF, 0x7C, 0x00, 0x3C, 0x3C };
+//char Status7[12]   = { 0xF9, 0x16, 0x03, 0xBF, 0xBF, 0x7C, 0xFC, 0x03, 0x80, 0x00, 0x3C, 0x3C };
+
 
 void setup() {
   pinMode(0,OUTPUT);
   pinMode(1,OUTPUT);
   pinMode(2,OUTPUT);
-  
+
   TCCR1 = 0b01010001;  // set COM1A0 PWM1A CS10  COM1A0 tells the timer to Toggle the value on OC1A pins (page 89 attiny25 datasheet)
 			//                       CS10 set timer prescale to 1
 			 //            PWM1A enable pwm output
@@ -71,40 +80,40 @@ void loop() {
   digitalWrite(2,LOW);
   while (true) { // this while is not needed
     messages();
-    
+
     if (sendStatus) {
 
       sendData(Status1);
       delayMicroseconds(2000+random(1000));
       messages();
-    
+
       sendData(Status2);
       delayMicroseconds(2000+random(1000));
       messages();
-    
+
       sendData(Status3);
       delayMicroseconds(2000+random(1000));
       messages();
-    
+
       sendData(Status4);
       delayMicroseconds(2000+random(1000));
       messages();
-    
+
       sendData(Status5);
       delayMicroseconds(2000+random(1000));
       messages();
-    
+
       sendData(Status6);
       delayMicroseconds(2000+random(1000));
       messages();
-    
+
       sendData(Status7);
       delayMicroseconds(2000+random(1000));
     }
   }
-  
-  
-  
+
+
+
   //delay(100);
 }
 
@@ -119,68 +128,68 @@ void sendByte(char inData) {
   if( (inData & (1 << 7)) ) {
     TCCR1 = timerstop;
   }
-  TCCR1 = timerstart; //  
+  TCCR1 = timerstart; //
   cycle_delay();
 
   if( (inData & (1 << 6)) ) {
     TCCR1 = timerstop;
   }
-  TCCR1 = timerstart; //  
+  TCCR1 = timerstart; //
   cycle_delay();
 
-  
+
   if( (inData & (1 << 5)) ) {
     TCCR1 = timerstop;
   }
-  TCCR1 = timerstart; //  
+  TCCR1 = timerstart; //
   cycle_delay();
 
-  
+
   if( (inData & (1 << 4)) ) {
     TCCR1 = timerstop;
   }
-  TCCR1 = timerstart; //  
+  TCCR1 = timerstart; //
   cycle_delay();
 
-  
+
   if( (inData & (1 << 3)) ) {
     TCCR1 = timerstop;
   }
-  TCCR1 = timerstart; //  
+  TCCR1 = timerstart; //
   cycle_delay();
 
-  
+
   if( (inData & (1 << 2)) ) {
     TCCR1 = timerstop;
   }
-  TCCR1 = timerstart; 
+  TCCR1 = timerstart;
   cycle_delay();
 
-  
+
   if( (inData & (1 << 1)) ) {
     TCCR1 = timerstop;
   }
-  TCCR1 = timerstart; 
+  TCCR1 = timerstart;
   cycle_delay();
 
-  
+
   if( (inData & (1 << 0)) ) {
     TCCR1 = timerstop;
   }
-  TCCR1 = timerstart; 
+  TCCR1 = timerstart;
   cycle_delay();
 
   // TODO: look if the last delay is to long with the return calls
- 
+
 }
 void sendData(char dataArray[]) {
   digitalWrite(2,HIGH); // stop led while sending data
-  
+
   TCCR1 = timerstart;
 
 
   noInterrupts();
-  
+
   sendByte(0x00);
   sendByte(dataArray[0]);
   sendByte(dataArray[1]);
@@ -194,16 +203,15 @@ void sendData(char dataArray[]) {
   sendByte(dataArray[9]);
   sendByte(dataArray[10]);
   sendByte(dataArray[11]);
-  
+
   TCCR1 = timerstop; //out  TCCR1,timerstop // Shut down radio
                      //ldi  slask, 0b0100_0000 // set COM1A0 (bit4) to 0 to disconnect timer pins
   TCCR1 = 0b01000000;//out  TCCR1,slask
   interrupts();
 
-  
-  
+
+
   digitalWrite(0,LOW);//cbi  PORTB, 0  // set both output to 0 to avoid current drain
   digitalWrite(1,LOW);//cbi  PORTB, 1  // set both output to 0 to avoid current drain
-  
-}
 
+}
